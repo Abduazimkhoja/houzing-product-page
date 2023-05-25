3442c6056e782603e4deb50f0485c88566d1b449
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-25 11:05:02 +0500

    component body

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from "react";
+import Feature from "./feature";
+import { ContainerMini, Line } from "./style";
+import Accommodation from "./accommodation";
+import Details from "./details";
+import Application from "./application";
+import Location from "./location";
 
-export default function Lesson() {
-  return (
-    <div>Lesson</div>
-  )
+export default function Body() {
+   return (
+      <>
+         {/* <Accommodation /> */}
+         <ContainerMini>
+            <Line />
+            <Location/>
+            <Line />
+            <Details />
+            <Line />
+            <Feature />
+            <Line />
+            <Application />
+            <Line />
+         </ContainerMini>
+      </>
+   );
 }
-
--- src/components/Body/style.js
+++ src/components/Body/style.js
@@ -1,1 +1,52 @@
 import styled, { css, keyframes } from "styled-components";
+
+export const Line = styled.div`
+   margin: 48px 0;
+   border-bottom: 1px solid #e6e9ec;
+`;
+
+export const ContainerMini = styled.div`
+   width: 880px;
+`;
+
+export const SectionTitle = styled.h5`
+   margin-bottom: 24px;
+
+   font-weight: 600;
+   font-size: 18px;
+   line-height: 28px;
+
+   color: #0d263b;
+`;
+
+export const Stats = styled.div`
+   display: flex;
+   align-items: center;
+   gap: 8px;
+
+   .stats__title {
+      font-weight: 600;
+      font-size: 14px;
+      line-height: 20px;
+
+      color: #0d263b;
+   }
+
+   .stats__info {
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 20px;
+
+      color: #696969;
+   }
+`;
+
+export const Grid = styled.div`
+   ${(props) => console.log(props.column)}
+
+   display: grid;
+   grid-template-columns: repeat(${(props) => props.column}, 1fr);
+   grid-template-rows: repeat(${(props) => props.row}, 1fr);
+   grid-column-gap: ${(props) => props.columnGap || "20px"};
+   grid-row-gap: ${(props) => props.rowGap || "24px"};
+`;
