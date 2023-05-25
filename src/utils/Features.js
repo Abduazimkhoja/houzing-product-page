abb6496b0c4ab7be7db527946d034f092e0e0cba
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-25 11:03:35 +0500

    utils feature

---
+++ src/utils/Features.js
@@ -0,0 +1,18 @@
+export const featuresList = [
+   { id: 1, icon: "conditioner", text: "Air Conditioning"},
+   { id: 2, icon: "grass", text: "Lawn"},
+   { id: 3, icon: "refrigerator", text: "Refrigerator"},
+   { id: 4, icon: "washer", text: "Washer"},
+   { id: 5, icon: "barbecue", text: "Barbeque"},
+   { id: 6, icon: "laundry", text: "Laundry"},
+   { id: 7, icon: "sauna", text: "Sauna"},
+   { id: 8, icon: "wifi", text: "WiFi"},
+   { id: 9, icon: "dryer", text: "Dryer"},
+   { id: 10, icon: "microwave", text: "Microwave"},
+   { id: 11, icon: "swimmer", text: "Swimming Pool"},
+   { id: 12, icon: "blinds", text: "Window Coverings"},
+   { id: 13, icon: "dumbbell", text: "Gym"},
+   { id: 14, icon: "outdoor-shower", text: "Outdoor Shower"},
+   { id: 15, icon: "coaxial", text: "TV Cable"},
+   { id: 16, icon: "chair", text: "Dining room"}
+]
