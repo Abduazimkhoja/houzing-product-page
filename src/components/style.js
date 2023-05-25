64527c78cde4bef50622caba57c9833cf9e6bb82
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-25 11:07:16 +0500

    grid, circleIcon ...

--- src/components/style.js
+++ src/components/style.js
@@ -15,3 +15,23 @@ export const Container = styled.div`
    height: inherit;
    margin: 0 auto;
 `;
+
+export const CircleIcon = styled.i`
+   width: 35px;
+   height: 35px;
+
+   display: flex;
+   align-items: center;
+   justify-content: center;
+
+   border-radius: 50%;
+   background: #f6f8f9;
+
+   transition: color 0.3s ease, background 0.3s ease;
+
+   &:hover {
+      color: #fff;
+      background: #0061df;
+      cursor: pointer;
+   }
+`;
