b1f1e9172d56c6ca3b41cdc39954c88019d4fc93
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-25 11:01:39 +0500

    Icon  reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Head from "./components/Header";
 import Foot from "./components/Footer";
+import Body from "./components/Body";
+import { Container } from "./components/style";
 
 export default function App() {
    return (
       <>
          <Head />
-         body
+         <Container>
+            <Body />
+         </Container>
          <Foot />
       </>
    );
