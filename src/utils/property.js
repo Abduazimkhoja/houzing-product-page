93390218391391842a5e9cdea7dbbb6ea5bf6188
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-25 11:04:14 +0500

    utils property

---
+++ src/utils/property.js
@@ -0,0 +1,12 @@
+export const PropertyDetailsName = [
+   { id: 1, text: "Property ID:", info: "PropertyID" },
+   { id: 2, text: "Bedrooms:", info: "beds" },
+   { id: 3, text: "Property Type:", info: "PropertyType" },
+   { id: 4, text: "Price:", info: "price" },
+   { id: 5, text: "Bathrooms:", info: "baths" },
+   { id: 6, text: "Property Status:", info: "PropertyStatus" },
+   { id: 7, text: "Property Size:", info: "area" },
+   { id: 8, text: "Garage:", info: "garages" },
+   { id: 9, text: "Year Build:", info: "YearBuilt" },
+   { id: 10, text: "Garage size:", info: "GarageSize" },
+];
