d8db8592dc1358db9e07b00e99f791bfbaa98099
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-25 11:02:55 +0500

    utils footerLIst

---
+++ src/utils/FooterList.js
@@ -0,0 +1,53 @@
+export const FooterList = [
+   {
+      id: 1,
+      title: "Contact Us",
+      icon: ["icon-location", "icon-phone", "icon-email"],
+      list: [
+         {
+            id: 1,
+            link: "329 Queensberry Street, North Melbourne VIC 3051, Australia.",
+         },
+         { id: 2, link: "123 456 7890" },
+         { id: 3, link: "support@houzing.com" },
+      ],
+      social: [
+         { id: 1, brand: "facebook" },
+         { id: 2, brand: "twitter" },
+         { id: 3, brand: "instagram" },
+         { id: 4, brand: "in" },
+      ],
+   },
+   {
+      id: 2,
+      title: "Discover",
+      list: [
+         { id: 1, link: "Chicago" },
+         { id: 2, link: "Los Angeles" },
+         { id: 3, link: "Miami" },
+         { id: 4, link: "New York" },
+      ],
+   },
+   {
+      id: 3,
+      title: "Lists by Category",
+      list: [
+         { id: 1, link: "Apartments" },
+         { id: 2, link: "Condos" },
+         { id: 3, link: "Houses" },
+         { id: 4, link: "Offices" },
+         { id: 5, link: "Retail" },
+         { id: 6, link: "Villas" },
+      ],
+   },
+   {
+      id: 4,
+      title: "Lists by Category",
+      list: [
+         { id: 1, link: "About Us" },
+         { id: 2, link: "Terms & Conditions" },
+         { id: 3, link: "Support Center," },
+         { id: 4, link: "Contact Us" },
+      ],
+   },
+];
