e3672cef3ed149957722db4c7aed5018d1685367
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-25 11:03:56 +0500

    utils location info

---
+++ src/utils/locationInfo.js
@@ -0,0 +1,8 @@
+export const LocationInfo = [
+   { id: 1, text: "Address:", info: "address" },
+   { id: 2, text: "City:", info: "city" },
+   { id: 3, text: "Area:", info: "area" },
+   { id: 4, text: "State/County: ", info: "state" },
+   { id: 5, text: "Zip:", info: "zip" },
+   { id: 6, text: "Country:", info: "country" },
+];
